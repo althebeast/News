49d1ef733c3db75aab81bd6d2346157d436eae68
Alperen <alperensarisan@icloud.com>
2024-02-27 16:26:33 -0400

    Initial Commit

---
+++ News/NewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  NewsApp.swift
+//  News
+//
+//  Created by Alperen Sarışan on 27.02.2024.
+//
+
+import SwiftUI
+
+@main
+struct NewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
